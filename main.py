fc88666073693f9070b0047c66c9479618739463
DisitaZX <DisitaZX@yandex.ru>
2021-04-25 23:09:19 +0500

    FULL_PROJECT V.0.2

--- main.py
+++ main.py
@@ -170,4 +170,5 @@ def bookname(book):
 
 if __name__ == '__main__':
     db_session.global_init('db/blogs.db')
-    app.run(port='8000', host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
